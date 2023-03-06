99c4069c9424b23d06b93cdac0077466047674cf
Jake Espinosa <jacob.sa.espinosa@gmail.com>
2023-03-05 17:03:12 -0700

    Add project files
    Setting up CI/CD with GitHub Actions.

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const URL = "https://lvbylmj5md.execute-api.us-west-1.amazonaws.com/cloudResumeChallenge/visitorcount"
+const httpRequest = new XMLHttpRequest();
+
+function handler() {
+    if (httpRequest.readyState === XMLHttpRequest.DONE) {
+        if (httpRequest.status === 200) {
+            response = httpRequest.responseText;
+            let responseObj = JSON.parse(response);
+            let text = responseObj['visitorCount']
+            
+            const para = document.createElement('p');
+            const node = document.createTextNode("Visitor Count: " + text);
+            para.appendChild(node);
+            const element = document.getElementById("div1");
+            element.appendChild(para);
+
+          } else {
+            alert("There was a problem with the request.");
+          }
+        }
+    }
+  
+httpRequest.onreadystatechange = handler;
+
+httpRequest.open("GET", URL);
+httpRequest.send();
